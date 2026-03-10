a1190fbb6ac7d973572d25aff4b19a638cabfc1b
beamendivil <bea@curatedtucson.com>
2026-03-10 11:24:35 -0700

    {fixes to user id req}

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ app.use(express.json());
 // request logger
 app.use(requestLogger);
 
+app.use((req, res, next) => {
+  req.user = { _id: '5d8b8592978f8bd833ca8133' };
+  next();
+});
+
 app.use('/', mainRouter);
 
 app.use((req, res, next) => {
