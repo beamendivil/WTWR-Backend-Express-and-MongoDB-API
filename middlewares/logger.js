33be7a8ebfff4dff617976cb999b4aea5ca272a3
beamendivil <bea@curatedtucson.com>
2026-04-12 10:08:46 -0700

    Fix logger middleware lint issues

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,42 +1,22 @@
 const winston = require("winston");
 const expressWinston = require("express-winston");
 
-const messageFormat = winston.format.combine(
-  winston.format.colorize({ all: true }),
-  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-  winston.format.printf(
-    ({ level, message, timestamp }) => `${timestamp} ${level}: ${message}`
-  )
-);
-
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.Console({
-      format: messageFormat,
-    }),
     new winston.transports.File({
       filename: "request.log",
-      format: winston.format.combine(
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        winston.format.json()
-      ),
     }),
   ],
+  format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.Console({
-      format: messageFormat,
-    }),
     new winston.transports.File({
       filename: "error.log",
-      format: winston.format.combine(
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        winston.format.json()
-      ),
     }),
   ],
+  format: winston.format.json(),
 });
 
 module.exports = {
