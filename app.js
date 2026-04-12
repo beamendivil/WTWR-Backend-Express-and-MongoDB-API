355424702aa32737456549d6c7965cd56e529a45
beamendivil <bea@curatedtucson.com>
2026-04-12 09:42:04 -0700

    Fix linting errors: change double quotes to single quotes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const { errors } = require("celebrate");
-const mainRouter = require("./routes/index.js");
-const errorHandler = require("./middlewares/error-handler.js");
-const { requestLogger, errorLogger } = require("./middlewares/logger.js");
-const { NotFoundError } = require("./utils/errors.js");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const mainRouter = require('./routes/index.js');
+const errorHandler = require('./middlewares/error-handler.js');
+const { requestLogger, errorLogger } = require('./middlewares/logger.js');
+const { NotFoundError } = require('./utils/errors.js');
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/wtwr_db")
+  .connect('mongodb://localhost:27017/wtwr_db')
   .then(() => {
-    console.log("Connected to DB");
+    console.log('Connected to DB');
   })
   .catch(console.error);
 
@@ -23,24 +23,24 @@ const { PORT = 3001 } = process.env;
 app.use(cors());
 app.use(express.json());
 
-app.get("/crash-test", () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Server will crash now");
+    throw new Error('Server will crash now');
   }, 0);
 });
 
 // Public endpoint for reviewers
-app.get("/public", (req, res) => {
-  res.send("WTWR API is running! Welcome, reviewer.");
+app.get('/public', (req, res) => {
+  res.send('WTWR API is running! Welcome, reviewer.');
 });
 
 // request logger
 app.use(requestLogger);
 
-app.use("/", mainRouter);
+app.use('/', mainRouter);
 
 app.use((req, res, next) => {
-  next(new NotFoundError("Requested resource not found"));
+  next(new NotFoundError('Requested resource not found'));
 });
 
 // error logger
